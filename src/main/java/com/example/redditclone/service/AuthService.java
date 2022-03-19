fd06be639ef80ca0d56bf0907f52a54fd9ea16c5
Mohssine Falih <mohssine.falih@adesso.de>
2022-03-19 17:11:59 +0100

    zurück das richtigste

--- src/main/java/com/example/redditclone/service/AuthService.java
+++ src/main/java/com/example/redditclone/service/AuthService.java
@@ -12,7 +12,8 @@ import java.time.Instant;
 @Service
 public class AuthService {
 
-
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     /**da es jetzt Password unserer User user in Datenbank zu spreichern
      * dafür brauchen wir UserRepository*/
